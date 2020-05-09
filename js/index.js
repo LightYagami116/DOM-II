e953b9af9ddd0dc4de29fcac3215180be784eabc
unknown <wastefuldragon@gmail.com>
2020-05-09 03:33:23 -0400

    done

--- js/index.js
+++ js/index.js
@@ -41,3 +41,46 @@ navStop.addEventListener('click', (e) => {
   e.preventDefault();
   alert(`Not Working!!`);
 });
+//keydown/keyup
+const x2 = document.querySelector('body');
+
+x2.addEventListener('keydown', (e) => {
+    e.target.style.opacity = 0;
+});
+x2.addEventListener('keyup', (e) => {
+    e.target.style.opacity = 1;
+});
+//mousedown/mouseup
+  x2.addEventListener('mousedown', (e) => {
+    e.target.style.background = '#000';
+    e.target.style.color = '#fff';
+  });
+  x2.addEventListener('mouseup', (e) => {
+    e.target.style.background = '#fff';
+    e.target.style.color = '#000';
+  });
+//load
+window.addEventListener('load', () => {
+    console.log('Page Loaded!');
+});
+//resize
+var r = 0;
+window.addEventListener('resize', () => {
+  const num = r++;
+  console.log(`window resized ${num}`);
+});
+//wheel
+const scrollCount = document.querySelector('body');
+var m = 0;
+scrollCount.addEventListener('wheel', (e) => {
+  const sCount = m++;
+    e.stopPropagation;
+    console.log(`I scrolled ${sCount} pixels 😁`);
+});
+//selector
+var x5 = document.querySelector('body');
+
+x5.addEventListener('select', (e) => {
+    console.log('something selected');
+
+});
